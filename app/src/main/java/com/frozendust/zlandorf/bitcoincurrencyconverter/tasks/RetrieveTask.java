890496b397aa5b1c8b50eb6ce2fb6642de10635b
zlandorf <pmilian33@gmail.com>
2015-08-01 00:13:52 +0200

    cleanup

--- app/src/main/java/com/frozendust/zlandorf/bitcoincurrencyconverter/tasks/RetrieveTask.java
+++ app/src/main/java/com/frozendust/zlandorf/bitcoincurrencyconverter/tasks/RetrieveTask.java
@@ -6,8 +6,5 @@ import com.frozendust.zlandorf.bitcoincurrencyconverter.models.entities.Rate;
 
 import java.util.List;
 
-/**
- * Created by zlandorf on 31/07/2015.
- */
 public abstract class RetrieveTask extends AsyncTask<Void, Void, List<Rate>> {
 }
